188dd9d619627e7b5a8a4b9836c044dc95f5f399
Ubuntu <ubuntu@ip-172-31-38-33.ec2.internal>
2021-05-14 09:28:59 +0000

    added Dockerfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { cribs } from './data/cribs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  cribs: any;
+  
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com/posts/1'; 
+  readonly ROOT_URL2 = 'https://ki7ulj4aa3.execute-api.us-east-1.amazonaws.com/dev/pets';
+  
+  constructor(private http: HttpClient) {
+    this.cribs = http.get(this.ROOT_URL2);
+    // this.cribs = [{"a": 1},{"b": 2}];
+  }
 }
